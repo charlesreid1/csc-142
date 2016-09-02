4afc1b841e4373044ef17e6615b6dac38fb93e88
Charles Reid <charlesreid1@gmail.com>
2016-09-02 14:32:28 -0700

    adding hello escape

---
+++ 0_HelloWorld/HelloEscape.java
@@ -0,0 +1,7 @@
+public class HelloEscape {
+    public static void main(String[] args) {
+        System.out.println("The following joke was approved by the Committee for Really Awful Programming Jokes:\n");
+        System.out.println("\tA programmer walks into a bar and says, \"Hey bartender, give me a double!\"");
+        System.out.println("\tThe bartender says, \"A float would be faster!\"");
+    }
+}
