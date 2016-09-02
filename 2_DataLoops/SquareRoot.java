7fa5cc1c19b8e9e5ccec2a8a282c6971cd38837f
Charles Reid <charlesreid1@gmail.com>
2016-09-02 15:48:54 -0700

    have to remember to print out iterations.

--- 2_DataLoops/SquareRoot.java
+++ 2_DataLoops/SquareRoot.java
@@ -1,23 +1,27 @@
 public class SquareRoot {
+
     public static void main(String[] args) { 
         double n = 55;
-        double sqrtn = approxSquareRoot(n);
-        System.out.printf("The square root of %d is approximately %4f",n,sqrtn);
+        approxSquareRoot(n);
     }
 
-    // approximate the square root
+    // function to approximate the square root using Newton's Method
     public static double approxSquareRoot(double quantity) { 
         // no imaginary numbers!
         if(quantity < 0) {
             break;
         }
 
+        System.out.printf("Approximating square root of %2f \n",quantity);
+
         // iterate a fixed number of times 
         int Niterations = 6;
         double value = quantity;
+        System.out.println("Iteration\tApproximation\n");
         for( int i=0; i < Niterations; i++ ) {
             // approximate the value of the square root using Newton's method
             value *= (quantity/2);
+            System.out.printf("%d\t%6f\n",i,value);
         }
     }
 }
