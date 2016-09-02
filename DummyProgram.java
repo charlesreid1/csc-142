b61b43605ff2fa7249fe299b26032544ebbf20b4
Charles Reid <charlesreid1@gmail.com>
2016-09-02 15:39:44 -0700

    adding a dummy program so we have comment templates.

---
+++ DummyProgram.java
@@ -0,0 +1,20 @@
+/*
+ * Dummy Program
+ *
+ * Problem:
+ * This is where you state what problem this program attempts to solve.
+ *
+ * Background:
+ * This is where you give any information, such as formulas or references,
+ * that may help in understanding the problem context.
+ *
+ * Algorithm:
+ * Here you list any of the major steps involved. 
+ *
+ */
+public class DummyProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
