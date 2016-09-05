29d5973e5b80206344ca310d062ded38c18fc7ad
Charles Reid <charlesreid1@gmail.com>
2016-09-05 15:06:06 -0700

    add confused operator

---
+++ 2_DataLoops/ConfusedOperator.java
@@ -0,0 +1,6 @@
+public class ConfusedOperator {
+    public static void main(String[] args) {
+        System.out.println( 2 + 3 + "hello" );
+        System.out.println( "hello" + 2 + 3 );
+    }
+}
