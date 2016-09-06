61e10f2c702bf1e95ebd2c86907633801c10e68e
Charles Reid <charlesreid1@gmail.com>
2016-09-05 23:10:24 -0700

    adding square root program

--- 2_DataLoops/SquareRoot.java
+++ 2_DataLoops/SquareRoot.java
@@ -1,28 +1,42 @@
+/**
+ * SquareRoot:
+ * Implement Newton's Method to obtain an approximation 
+ * for the square root of b.
+ *
+ * The approximation of the square root of b is obtained through
+ * the iterative approximation:
+ *
+ *      x_{i+1} = x_{i} + ( x_{i}^2 - b )/(2 x_i)
+ *
+ */
 public class SquareRoot {
 
     public static void main(String[] args) { 
-        double n = 55;
-        approxSquareRoot(n);
+        double b = 55;
+        int Niter = 10;
+        approxSquareRoot(b,Niter);
     }
 
-    // function to approximate the square root using Newton's Method
-    public static double approxSquareRoot(double quantity) { 
-        // no imaginary numbers!
-        if(quantity < 0) {
-            break;
+    /** Approximate the square root of b using Newton's Method */
+    public static double approxSquareRoot(double b, int Niterations) {
+
+        // No imaginary numbers!
+        if(b < 0) {
+            return -666;
         }
 
-        System.out.printf("Approximating square root of %2f \n",quantity);
+        System.out.println("Approximating square root of " + b);
 
-        // iterate a fixed number of times 
-        int Niterations = 6;
-        double value = quantity;
-        System.out.println("Iteration\tApproximation\n");
-        for( int i=0; i < Niterations; i++ ) {
-            // approximate the value of the square root using Newton's method
-            value *= (quantity/2);
-            System.out.printf("%d\t%6f\n",i,value);
+        // Iterate a fixed number of times 
+        double truth = Math.sqrt(b);
+        double x_i = b;
+        System.out.print("Iter\tApprox\tTruth\n");
+
+        // approximate the value of the square root using Newton's method
+        for( int i=1; i <= Niterations; i++ ) {
+            x_i -= ( Math.pow(x_i,2) - b )/(2 * x_i );
+            System.out.printf("%d\t%6f\t%6f\n",i,x_i,truth);
         }
+        return x_i;
     }
 }
-
