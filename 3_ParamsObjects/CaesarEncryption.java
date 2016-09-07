8357417d6c18a9da7bb72b7de0741ffc951a034f
Charles Reid <charlesreid1@gmail.com>
2016-09-06 17:38:09 -0700

    adding socpe example and finishing caesar encryption.

--- 3_ParamsObjects/CaesarEncryption.java
+++ 3_ParamsObjects/CaesarEncryption.java
@@ -1,8 +1,20 @@
 public class CaesarEncryption {
     public static void main(String[] args) {
-        // char shift is the key
-        for(char c = 'A'; c <='Z'; c++) {
-            System.out.println( (char)(c - 12) );
+
+        String message = "gaulisdividedintothreeparts";
+
+        message = message.toUpperCase();
+        int key = 12; // zero-indexed
+        for( int i = 0; i < message.length(); i++ ) {
+            char c, c2;
+            int ix, ix2;
+
+            c = message.charAt(i);
+            ix = (c - 'A');
+            ix2 = (ix + key)%26; // Compute shift
+            c2 = (char)('A' + ix2);
+
+            System.out.print(c2);
         }
     }
 }
