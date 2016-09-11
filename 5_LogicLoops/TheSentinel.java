b9e77dc63ff3e042f1d064de7ca392082d454b32
Charles Reid <charlesreid1@gmail.com>
2016-09-11 15:45:45 -0700

    adding sentinel program for chapter 5

---
+++ 5_LogicLoops/TheSentinel.java
@@ -0,0 +1,14 @@
+public class TheSentinel {
+    public static void main(String[] args) {
+        //
+        // Drunkard's Walk
+        //
+        // Starting point is origin (0,0)
+        //
+        // start program
+        // ask usre for next input: how many steps to take? 
+        // ask user for next input and validate input
+        // sentinel value?
+        //
+    }
+}
