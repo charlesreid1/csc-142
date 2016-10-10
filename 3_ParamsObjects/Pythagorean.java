57da320bb9698386f6976cb25ec137cdf6307ac0
Charles Reid <charlesreid1@gmail.com>
2016-10-09 23:36:36 -0700

    updating pythagorean theorem code

--- 3_ParamsObjects/Pythagorean.java
+++ 3_ParamsObjects/Pythagorean.java
@@ -1,7 +1,14 @@
 public class Pythagorean {
     public static void main(String[] args) {
-        c = findHypotenuse(a, b);
-        System.out.printf("Hypotensue length = %4f",c);
+        double a = 8.0;
+        double b = 9.0;
+        double c = findHypotenuse(a, b);
+        System.out.printf("Hypotensue length = %.4f",c);
+    }
+
+    public static double findHypotenuse(double a, double b) {
+        return Math.sqrt(a*a + b*b);
     }
 }
+
 
