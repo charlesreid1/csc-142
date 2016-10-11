e19eea6ee6f585faa337c5393185403a00874eaa
Charles Reid <charlesreid1@gmail.com>
2016-10-10 20:54:37 -0700

    fix typo in quadratic formula

--- 3_ParamsObjects/QuadraticEquation.java
+++ 3_ParamsObjects/QuadraticEquation.java
@@ -8,7 +8,7 @@ public class QuadraticEquation {
 
     public static void main(String[] args) {
         printIntro();
-        solveQuadraticEquation(1.0,4.0,4.0);
+        solveQuadraticEquation(2.0,8.0,7.0);
         solveQuadraticEquation(1.0,2.0,1.0);
     }
 
@@ -31,11 +31,17 @@ public class QuadraticEquation {
         if(det < 0) {
             System.out.println("Negative determinant: No real roots");
             return;
+            // NOTE: We can do this instead of raising an exception, 
+            // since the user calling solveQuadraticEquation is not expecting 
+            // a solution to be returned.
+            //
+            // If the user depended on a number being returned, 
+            // we would have to raise an exception.
         } 
 
         double x1, x2;
-        x1 = (  - b + Math.sqrt( Math.pow(b,2) - 4*a*c )  )/2;
-        x2 = (  - b - Math.sqrt( Math.pow(b,2) - 4*a*c )  )/2;
+        x1 = (  - b + Math.sqrt( Math.pow(b,2) - 4*a*c )  )/(2*a);
+        x2 = (  - b - Math.sqrt( Math.pow(b,2) - 4*a*c )  )/(2*a);
 
         System.out.println("Solutions found!");
         System.out.printf("x1 = %4f \n",x1);
