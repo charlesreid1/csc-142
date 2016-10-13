ea1ad88116e068a304818b103f96695900a82697
Charles Reid <charlesreid1@gmail.com>
2016-10-13 08:05:00 -0700

    adding loop to explore impact of initial guess

--- 2_DataLoops/SquareRoot.java
+++ 2_DataLoops/SquareRoot.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * SquareRoot:
  * Implement Newton's Method to obtain an approximation 
@@ -12,30 +13,36 @@
 public class SquareRoot {
 
     public static void main(String[] args) { 
-        double b = 55;
-        int Niter = 10;
-        approxSquareRoot(b,Niter);
+        double b = 2;
+        for(int i=1; i<100; i*=2) {
+            double initial_guess = i*b;
+            int Niter = 10;
+            approxSquareRoot(b,initial_guess,Niter);
+            System.out.println();
+        }
     }
 
     /** Approximate the square root of b using Newton's Method */
-    public static double approxSquareRoot(double b, int Niterations) {
+    public static double approxSquareRoot(double b, double initial_guess, int Niterations ) {
 
         // No imaginary numbers!
         if(b < 0) {
-            return -666;
+            throw new IllegalArgumentException("ERROR: You cannot compute the square root of an imaginary number with this method.");
         }
 
         System.out.println("Approximating square root of " + b);
 
         // Iterate a fixed number of times 
         double truth = Math.sqrt(b);
-        double x_i = b;
-        System.out.print("Iter\tApprox\tTruth\n");
+        double x_i = initial_guess;
+        System.out.print("Iteration\tApproximation\t\tTrue Value\n");
+
+        System.out.printf("%2d\t\t%.10f\t\t%.10f\n",0,initial_guess,truth);
 
         // approximate the value of the square root using Newton's method
         for( int i=1; i <= Niterations; i++ ) {
             x_i -= ( Math.pow(x_i,2) - b )/(2 * x_i );
-            System.out.printf("%d\t%6f\t%6f\n",i,x_i,truth);
+            System.out.printf("%2d\t\t%.10f\t\t%.10f\n",i,x_i,truth);
         }
         return x_i;
     }
