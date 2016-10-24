5884e19463251486566571f3d65e943b403b7ae5
Charles Reid <charlesreid1@gmail.com>
2016-10-23 23:52:43 -0700

    make sentinel class constant

--- 5_LogicLoops/TheSentinel.java
+++ 5_LogicLoops/TheSentinel.java
@@ -1,20 +1,26 @@
 import java.util.*;
 
 public class TheSentinel {
+    public static final int SENTINEL = -1;
+
     public static void main(String[] args) {
+        
         Scanner console = new Scanner(System.in);
         int sum = 0;
         
         // pull one prompt/read ("post") out of the loop
-        System.out.print("Enter a number (-1 to quit): ");
+        System.out.print("Enter a number (" + SENTINEL +
+                         " to quit): ");
         int number = console.nextInt();
         
-        while (number != -1) {
+        while (number != SENTINEL) {
             sum = sum + number;     // moved to top of loop
-            System.out.print("Enter a number (-1 to quit): ");
+            System.out.print("Enter a number (" + SENTINEL +
+                             " to quit): ");
             number = console.nextInt();
         }
         
         System.out.println("The total is " + sum);
+
     }
 }
